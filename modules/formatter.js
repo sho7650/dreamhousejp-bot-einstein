a30bfc4a9160ea742f42479553d2d73169f644a1
ccoenraets <ccoenraets@gmail.com>
2016-04-25 11:27:58 -0400

    fix

--- modules/formatter.js
+++ modules/formatter.js
@@ -49,39 +49,73 @@ let formatAppointment = property => {
         moment().add(3, 'days').format('ddd MMM Do') + ' at 1pm',
         moment().add(3, 'days').format('ddd MMM Do') + ' at 6pm',
     ];
-    let elements = [];
-    elements.push({
-        title: "Scheduling a visit",
-        subtitle: property.get("Address__c") + " " + property.get("City__c") + " " + property.get("State__c") + " · " + property.get("Price__c"),
-        "image_url": property.get("Picture__c"),
-        "buttons": [
-            {
-                "type": "postback",
-                "title": options[0],
-                "payload": "confirm_visit," + property.get("Address__c") + " in " + property.get("City__c") + "," + options[0]
-            },
-            {
-                "type": "postback",
-                "title": options[1],
-                "payload": "confirm_visit," + property.get("Address__c") + " in " + property.get("City__c") + "," + options[1]
-            },
-            {
-                "type": "postback",
-                "title": options[2],
-                "payload": "confirm_visit," + property.get("Address__c") + " in " + property.get("City__c") + "," + options[2]
-            }
-        ]
-    });
     return {
         "attachment": {
             "type": "template",
             "payload": {
-                "template_type": "generic",
-                "elements": elements
+                "template_type": "button",
+                "text": `Scheduling a visit at ${property.get("Address__c")} in ${property.get("City__c")}. Select one of the available appointments below:`,
+                "buttons": [
+                    {
+                        "type": "postback",
+                        "title": options[0],
+                        "payload": "confirm_visit," + property.get("Address__c") + " in " + property.get("City__c") + "," + options[0]
+                    },
+                    {
+                        "type": "postback",
+                        "title": options[1],
+                        "payload": "confirm_visit," + property.get("Address__c") + " in " + property.get("City__c") + "," + options[1]
+                    },
+                    {
+                        "type": "postback",
+                        "title": options[2],
+                        "payload": "confirm_visit," + property.get("Address__c") + " in " + property.get("City__c") + "," + options[2]
+                    }]
             }
         }
     };
 };
+//let formatAppointment = property => {
+//    var options = [
+//        moment().add(1, 'days').format('ddd MMM Do') + ' at 10am',
+//        moment().add(2, 'days').format('ddd MMM Do') + ' at 9am',
+//        moment().add(2, 'days').format('ddd MMM Do') + ' at 5pm',
+//        moment().add(3, 'days').format('ddd MMM Do') + ' at 1pm',
+//        moment().add(3, 'days').format('ddd MMM Do') + ' at 6pm',
+//    ];
+//    let elements = [];
+//    elements.push({
+//        title: "Scheduling a visit",
+//        subtitle: property.get("Address__c") + " " + property.get("City__c") + " " + property.get("State__c") + " · " + property.get("Price__c"),
+//        "image_url": property.get("Picture__c"),
+//        "buttons": [
+//            {
+//                "type": "postback",
+//                "title": options[0],
+//                "payload": "confirm_visit," + property.get("Address__c") + " in " + property.get("City__c") + "," + options[0]
+//            },
+//            {
+//                "type": "postback",
+//                "title": options[1],
+//                "payload": "confirm_visit," + property.get("Address__c") + " in " + property.get("City__c") + "," + options[1]
+//            },
+//            {
+//                "type": "postback",
+//                "title": options[2],
+//                "payload": "confirm_visit," + property.get("Address__c") + " in " + property.get("City__c") + "," + options[2]
+//            }
+//        ]
+//    });
+//    return {
+//        "attachment": {
+//            "type": "template",
+//            "payload": {
+//                "template_type": "generic",
+//                "elements": elements
+//            }
+//        }
+//    };
+//};
 
 let formatBroker = broker => {
     let elements = [];
