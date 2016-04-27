7380bfb451ebd451b5027bb93e3987bbe6ec31e0
ccoenraets <ccoenraets@gmail.com>
2016-04-26 23:08:26 -0400

    fix

--- modules/messenger.js
+++ modules/messenger.js
@@ -22,11 +22,12 @@ exports.send = (message, recipient) => {
 
 exports.getUserInfo = (userId) => {
 
+    console.log("##### getUserInfo");
     return new Promise((resolve, reject) => {
 
         request({
             url: `https://graph.facebook.com/v2.6/${userId}`,
-            qs: {fields: first_name, last_name, profile_pic, access_token: process.env.PAGE_TOKEN},
+            qs: {fields:"first_name,last_name,profile_pic", access_token: process.env.PAGE_TOKEN},
             method: 'GET',
         }, (error, response) => {
             console.log("#### getUserInfo");
