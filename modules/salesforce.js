31304da320baaf9376a1c769fa7632913c13fd4e
Takashi Abe <tabe@salesforce.com>
2017-06-28 14:56:12 +0900

    Rollback authentication method

--- modules/salesforce.js
+++ modules/salesforce.js
@@ -2,30 +2,28 @@
 
 let nforce = require('nforce'),
 
-    SF_CLIENT_ID = process.env.SF_CLIENT_ID,
-    SF_CLIENT_SECRET = process.env.SF_CLIENT_SECRET,
-    SF_USER_NAME = process.env.SF_USER_NAME,
-    SF_PASSWORD = process.env.SF_PASSWORD;
+  SF_CLIENT_ID = process.env.SF_CLIENT_ID,
+  SF_CLIENT_SECRET = process.env.SF_CLIENT_SECRET,
+  SF_USER_NAME = process.env.SF_USER_NAME,
+  SF_PASSWORD = process.env.SF_PASSWORD;
 
 let org = nforce.createConnection({
-    clientId: SF_CLIENT_ID,
-    clientSecret: SF_CLIENT_SECRET,
-    apiVersion: '36.0',
-    envirionment: 'production',
-    redirectUri: 'http://localhost:3000/oauth/_callback',
-    mode: 'single'
-//    autoRefresh: true
+  clientId: SF_CLIENT_ID,
+  clientSecret: SF_CLIENT_SECRET,
+  redirectUri: 'http://localhost:3000/oauth/_callback',
+  mode: 'single',
+  autoRefresh: true
 });
 
 let login = () => {
-    org.authenticate({username: SF_USER_NAME, password: SF_PASSWORD}, function(err, resp) {
-        if (err) {
-            console.error("Authentication error");
-            console.error(err);
-        } else {
-            console.log("Authentication successful");
-        }
-    });
+  org.authenticate({username: SF_USER_NAME, password: SF_PASSWORD}, err => {
+    if (err) {
+        console.error("Authentication error");
+        console.error(err);
+    } else {
+        console.log("Authentication successful");
+    }
+  });
 };
 
 let findProperties = (params) => {
